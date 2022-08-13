5356709f01ed061864de4d4a88d42c51a4036fab
Anh Do <36804143+Anh-tp-do@users.noreply.github.com>
2022-08-13 16:45:21 -0500

    Update Two-to-Four-decoder.v

--- Two-to-Four-decoder.v
+++ Two-to-Four-decoder.v
@@ -3,9 +3,9 @@ module decoder (
  output [3:0] D
  );
  
- assign D[0] = !I0 & !I1 & !En;
- assign D[1] = I0 & !I1 & !En;
- assign D[2] = !I0 & I1 & !En;
- assign D[3] = I0 & I1 & !En;
+ assign D[0] = !I0 && !I1 && !En;
+ assign D[1] = I0 && !I1 && !En;
+ assign D[2] = !I0 && I1 && !En;
+ assign D[3] = I0 && I1 && !En;
 
 endmodule
