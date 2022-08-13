f310903bd0c8e8d7a15333358583235f9ecc34ac
Anh Do <36804143+Anh-tp-do@users.noreply.github.com>
2022-08-13 16:44:48 -0500

    Create Two-to-Four-decoder.v

---
+++ Two-to-Four-decoder.v
@@ -0,0 +1,11 @@
+module decoder (
+ input I0, I1, En,
+ output [3:0] D
+ );
+ 
+ assign D[0] = !I0 & !I1 & !En;
+ assign D[1] = I0 & !I1 & !En;
+ assign D[2] = !I0 & I1 & !En;
+ assign D[3] = I0 & I1 & !En;
+
+endmodule
